e1d54f6587a9498c048e311e05ab688a4521fd54
Ackfort <phattarakorn23228@gmail.com>
2024-04-05 17:14:15 +0700

    create UserRouter

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -17,8 +18,4 @@ app.listen(3000, () => {
     console.log('DONE');
 });
 
-app.get('/test', (req,res) => {
-    res.json({
-        message: 'API is Work'
-    });
-});
+app.use('/api/user', userRoutes);
